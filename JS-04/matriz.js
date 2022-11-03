7a5328027704f3a49c41aaed3a62834dfeb14be7
julissajarking <julissa.jarkin@gmail.com>
2022-11-03 17:23:57 -0600

    Ejercicios de matrices

---
+++ JS-04/matriz.js
@@ -0,0 +1,35 @@
+let arr= [
+    
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+
+
+
+/* let suba=[1,2,3];
+let subb=[4,5,6];
+let subc=[7,8,9];
+let arregloS=[suba,subb,subc]; */
+
+/*  for (let fila= 0; fila <= 2; fila++)
+{
+    
+} */
+/* let contador =0; */
+
+/* let fila=0;
+do {
+   for( let columna=0; columna <=2; columna++ )
+    {
+        console.log(arr[fila][columna]);
+    } 
+    fila++;
+    console.log("---------"); 
+   
+}  while(fila < 3) */
+/* do{
+    
+    console.log(contador);
+    contador ++;
+}while (contador<=10); */
