09fd550c8ff0b404d47f7079990aeb9a3f6345f1
julissajarking <julissa.jarkin@gmail.com>
2022-12-21 19:00:49 -0600

    jwt apiUsuario

---
+++ java-web-1/ejemplo0-4jwt/src/test/java/com/generation/jxjg/myappjwt/Ejemplo04jwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.jxjg.myappjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejemplo04jwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
